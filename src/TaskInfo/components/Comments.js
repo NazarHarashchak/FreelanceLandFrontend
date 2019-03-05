d1ef8852b3af7cd49ba9ae1f6e7583681d75459f
NazarHarashchak <42061216+NazarHarashchak@users.noreply.github.com>
2019-03-05 12:57:37 +0200

    Added comments list (#15)

--- src/TaskInfo/components/Comments.js
+++ src/TaskInfo/components/Comments.js
@@ -1,23 +1,44 @@
 import React from "react";
 import Comment from "./Comment";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import {actionCommentsCreators} from "../commentAction"
 
 import "./comments.css";
 
 class Comments extends React.Component {
+  componentWillMount(){
+    this.props.requestComments(this.props.taskId);
+  }
   render() {
     return (
-      <div>
+      <div className="comments-panel">
       <div className="comments">
-      <ul>
+        <ul width="100%">
             <div id="comentar">
               <li>
-                <Comment date='22.03.2019' description='some commentar' user='Anton'/>
+            {this.props.comments.map(item => (          
+                <Comment
+                key={item.id}
+                item={item}
+                />
+            ))}
               </li>
             </div>
         </ul>
-      </div></div>
+      </div>
+      <div id="add-comment">
+        <form>
+              <input type="text" width ="100%"/>
+              <input type="submit" />
+        </form>
+        </div>
+      </div>
     );
   }
 }
 
-export default Comments;
+export default connect(
+  state => state.commentsTask,
+  dispatch => bindActionCreators(actionCommentsCreators, dispatch)
+)(Comments);
