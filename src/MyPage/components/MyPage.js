c2511d5fa316eb2ede0eeea3fd18b1afa15ced60
Andrii Buchman <buchman1808@gmail.com>
2019-03-14 17:28:37 +0200

    Added user task history

--- src/MyPage/components/MyPage.js
+++ src/MyPage/components/MyPage.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import Tabs from './Tabs';
 import MainPage from '../../mainPages/components/MainPage';
 import Login from '../../LoginPage/Login';
-
+import Tasks from '../../tasks/components/Tasks';
+import ProfileComponent from '../../ProfilePage/components/ProfileComponent';
+import PersonalInfo from '../../ProfilePage/components/PersonalInfo';
 require('./styles.css');
 class MyPage extends Component {
   
@@ -13,10 +15,10 @@ class MyPage extends Component {
         <h1>My Page</h1>
         <Tabs>
           <div label="Anctive tasks">
-            <MainPage></MainPage>
+            <Tasks></Tasks>
           </div>
           <div label="History">
-            <Login></Login>
+            <ProfileComponent id="6"></ProfileComponent>
           </div>
           <div label="My information">
             Nothing to see here, this tab is <em>extinct</em>!
