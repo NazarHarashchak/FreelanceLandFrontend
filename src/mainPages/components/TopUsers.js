786740558f27fcddfb27f9b7f086a30d84254a89
Andrii Buchman <buchman1808@gmail.com>
2019-04-18 18:45:19 +0300

    fix

--- src/mainPages/components/TopUsers.js
+++ src/mainPages/components/TopUsers.js
@@ -2,47 +2,71 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { requestTop } from '../actions';
-import PropTypes from 'prop-types';
-
 import '../topUsers.css';
+//
+
+//
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 
 
 class Gallery extends React.Component {
-    static propTypes = {
-        users:PropTypes.arrayOf(PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            name: PropTypes.string,
-            sur_Name: PropTypes.string
-        }))
-    }
-
     constructor(props) {
         super(props);
     }
-    
-    componentWillMount(){
-        requestTop();
-        }
 
-        
-    render(){
-          return(
-            <div className=" top-users">
-             {this.props.users.map(item  =>(
-                <li className="top-users-item" key={item.id}>
-                <h4>Top position : {item.id}</h4>
-                <h5>Name:{item.name}</h5> 
-                <h5>Surname:{item.sur_Name} </h5>
-                <h5>Rating:</h5>
-                </li>
-              ))}
-              </div>
-          )}};
+    items = [1, 2, 3, 4, 5]
+
+    state = {
+      currentIndex: 0,
+      responsive: { 1024: { items: 3 } },
+      galleryItems: this.galleryItems()
+    }
+   
+    slideTo = (i) => this.setState({ currentIndex: i })
+   
+    onSlideChanged = (e) => this.setState({ currentIndex: e.item })
+   
+    slideNext = () => this.setState({ currentIndex: this.state.currentIndex + 1 })
+   
+    slidePrev = () => this.setState({ currentIndex: this.state.currentIndex - 1 })
+   
+
+   
+    galleryItems() {
+      return this.props.users.map((i) => 
+      <li key={i.id}>
+        <h5>Name:{i.name}</h5> 
+        <h5>Surname:{i.sur_Name} </h5>
+        <h5>Rating: 10</h5>
+      </li>)
+    }
+   
+    render() {
+      const { galleryItems, responsive, currentIndex } = this.state
+      return (
+        <div>
+            <button onClick={() => this.slidePrev()}>Prev button</button>
+          <AliceCarousel
+            dotsDisabled={true}
+            buttonsDisabled={true}
+            items={this.state.galleryItems}
+            responsive={responsive}
+            slideToIndex={currentIndex}
+            onSlideChanged={this.onSlideChanged}
+          />
+          
+          <button onClick={() => this.slideNext()}>Next button</button>
+        </div>
+      )
+    }
+ 
+
+};
  
 
 
 export default connect(
-    state => state.topUsers,
-    dispatch => bindActionCreators(requestTop, dispatch)
+    state => state.topUsers, null
 )(Gallery);
